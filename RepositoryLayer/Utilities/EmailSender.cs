4a4370c5d87d1a610ed3886d2b5b6fbc1b82eeb6
Rohit Pagar <rohitpagar13@gmail.com>
2024-06-13 18:21:49 +0530

    [Rohit] Added: Forgot and Reset functionality added

---
+++ RepositoryLayer/Utilities/EmailSender.cs
@@ -0,0 +1,36 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using MimeKit.Text;
+using ModelLayer;
+using RepositoryLayer.Utilities.emailModel;
+
+namespace RepositoryLayer.Utilities
+{
+    public class EmailSender
+    {
+
+        public static void SendEmail(EmailModel emailModel, IConfiguration config)
+        {
+            var email = new MimeMessage();
+
+            email.From.Add(MailboxAddress.Parse(config.GetSection("EmailSettings:EmailUsername").Value));
+            email.To.Add(MailboxAddress.Parse(emailModel.To));
+            email.Subject = emailModel.Subject;
+            email.Body = new TextPart(TextFormat.Html)
+            {
+                Text = emailModel.Body
+            };
+
+
+            using var smtp = new SmtpClient();
+            smtp.Connect(config.GetSection("EmailSettings:EmailHost").Value, 587, SecureSocketOptions.StartTls);
+            smtp.Authenticate(config.GetSection("EmailSettings:EmailUsername").Value, Environment.GetEnvironmentVariable("emailpass(rohitpagar133@gmail.com)"));
+            smtp.Send(email);
+            smtp.Disconnect(true);
+            smtp.Dispose();
+        }
+    }
+}
---
+++ RepositoryLayer/Utilities/JWTTokenGenerator.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using RepositoryLayer.Entities;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryLayer.Utilities
+{
+    public class JWTTokenGenerator
+    {
+
+        public static string generateToken(int id, string email, IConfiguration _configuration)
+        {
+            var claims = new[] {
+                    new Claim(JwtRegisteredClaimNames.Sub, _configuration["Jwt:Subject"]),
+                    new Claim("Id", id.ToString()),
+                    new Claim("Email", email),
+                };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(10),
+                signingCredentials: signIn);
+            var jwttoken = new JwtSecurityTokenHandler().WriteToken(token);
+            return jwttoken;
+        }
+    }
+    }
+}
